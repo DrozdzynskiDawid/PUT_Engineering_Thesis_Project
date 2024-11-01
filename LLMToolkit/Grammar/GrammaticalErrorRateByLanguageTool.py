cd1f3e57e0a88d1b837da070f2ae49190cc67f5b
Dawid Drożdżyński <55097930+DrozdzynskiDawid@users.noreply.github.com>
2024-11-01 17:42:31 +0100

    Merge develop into main (#13)
    * update GrammaticalErrorRateByLanguageTool
    
    * fix
    
    * return all_matches

--- LLMToolkit/Grammar/GrammaticalErrorRateByLanguageTool.py
+++ LLMToolkit/Grammar/GrammaticalErrorRateByLanguageTool.py
@@ -2,11 +2,11 @@ from ..ModuleException import *
 import requests
 import nltk
 
-def check(text: str):
+def check(text: str, language: str = 'en-US'):
     api_url = "https://api.languagetool.org/v2/check"
     data = {
             'text': text,
-            'language': 'en-US',
+            'language': language,
         }
     response = requests.post(api_url, data=data)
     matches = response.json().get('matches', [])
@@ -19,10 +19,10 @@ def getGER(text: str):
     sentences = nltk.tokenize.sent_tokenize(text)
     sentences_length = len(sentences)
     count_errors = 0
-
+    all_matches = check(text)
     for s in sentences:
         matches = check(s)
         if matches:
             count_errors += 1
 
-    return count_errors / sentences_length * 100
+    return (count_errors / sentences_length * 100, all_matches)
