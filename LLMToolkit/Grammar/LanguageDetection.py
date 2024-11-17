6e4e3e972cebbbe6e3ee46ee8e023183e1e66424
Dawid Drożdżyński <55097930+DrozdzynskiDawid@users.noreply.github.com>
2024-11-17 12:35:50 +0100

    added language detection using lingua (#15) (#16)

---
+++ LLMToolkit/Grammar/LanguageDetection.py
@@ -0,0 +1,15 @@
+from lingua import Language, LanguageDetectorBuilder
+from ..ModuleException import *
+
+def getLanguageConfidenceValue(text: str, top: int):
+    if type(text) != str or type(top) != int:
+        getTypeError()
+    all_languages = Language.all()
+    detector = LanguageDetectorBuilder.from_languages(*all_languages).build()
+    confidence_values = detector.compute_language_confidence_values(text)
+    sorted_confidence_values = sorted(confidence_values, key=lambda c: c.value, reverse=True)
+    # get only top languages and compute values again
+    top_languages = [confidence.language for confidence in sorted_confidence_values[:top]]
+    top_detector = LanguageDetectorBuilder.from_languages(*top_languages).build()
+    top_confidence_values = top_detector.compute_language_confidence_values(text)
+    return top_confidence_values
--- LLMToolkit/Grammar/__init__.py
+++ LLMToolkit/Grammar/__init__.py
@@ -7,5 +7,6 @@ __all__ = [
     'PartOfSpeechTagging',
     'GrammaticalErrorRateByLanguageTool',
     'GrammaticalErrorRateByIKorektor',
-    'SpellChecker'
+    'SpellChecker',
+    'LanguageDetection'
 ]
