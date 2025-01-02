fe8ba07306caa4a889543f811050e85861a90def
Jakub Filipiak <kubafil15@wp.pl>
2025-01-02 12:46:53 +0100

    Fix and Add tests

--- LLMToolkit/Translation/Meteor.py
+++ LLMToolkit/Translation/Meteor.py
@@ -1,4 +1,20 @@
+from nltk.translate.meteor_score import meteor_score
+import nltk
+nltk.download('wordnet')
+
 def calculate_meteor(reference, hypothesis):
+    if (len(reference) == 0):
+        raise ValueError("Reference is empty")
+
+    if (len(hypothesis) == 0):
+        raise ValueError("Hypothesis is empty")
+
+    if type(reference) is list:
+        reference = [reference]
+
+    if type(reference) is not list:
+        reference = [reference.strip().split()]
+        
     score = meteor_score(reference, hypothesis)
     print(f"Meteor Score: {score}")
     return score
